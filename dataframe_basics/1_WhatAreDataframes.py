f7c1e1a10db5e254014ebb4cf8b67becc9baff28
ShadaMaayouf <maayouf27@gmail.com>
2023-09-07 16:06:47 +0100

    add pandas tutorial project

---
+++ dataframe_basics/1_WhatAreDataframes.py
@@ -0,0 +1,10 @@
+# a pandas dataframe is essentially a dictionary
+
+#this is a dictionary
+people = {
+    "first": ["Shatha", "ahmed", "laila"],
+    "last": ["Maayouf", "Fitouri", "Tili"],
+    "age": [24,35,10]
+}
+
+df =
